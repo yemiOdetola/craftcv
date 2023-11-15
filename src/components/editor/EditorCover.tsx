23acd12ae4189f900874971bc0d67f7c718ef958
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2023-11-15 22:45:33 +0100

    editor header

---
+++ src/components/editor/EditorCover.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+import { Menu } from '@/components/editor'
+
+export default function EditorCover({ children }: any) {
+  return (
+    <div className="w-full bg-white py-32">
+      <Menu />
+      <div className="w-full lg:w-4/6 min-h-screen mx-auto editor-container p-8 shadow shadow-gray-400 rounded-lg">
+        {children}
+      </div>
+    </div>
+  )
+}
