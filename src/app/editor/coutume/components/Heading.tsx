560a3ea439c22beb4bf34ad08371c849bc6b3067
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2023-12-18 17:38:37 +0100

    chore: remove item from path

--- src/app/editor/coutume/components/Heading.tsx
+++ src/app/editor/coutume/components/Heading.tsx
@@ -21,17 +21,14 @@ const adder = [
 
 export default function Heading({ children, id, className }: HeadingProps) {
   const [allowNew] = useState(adder.includes(id));
+  const [isHovered, setIsHovered] = useState(false);
   const [count, setCount] = useState<number>(1);
   const { saveWithPath } = useEditorActions();
 
-  // const getUniqueID = () => {
-  //   return Math.random().toString(36).substring(2, 15);
-  // };
-
   const addSection = () => {
     let sectionData: any = basetemplate[id];
+    const uid = Math.random().toString(36).substring(2, 15);
     sectionData = sectionData[Object.keys(sectionData)[0]];
-    let uid = Math.random().toString(36).substring(2, 15);
     sectionData.id = uid;
     if (allowNew) {
       setCount(count + 1);
@@ -40,13 +37,23 @@ export default function Heading({ children, id, className }: HeadingProps) {
   };
 
   return (
-    <div className='flex cursor-pointer items-center'>
-      <h2 className={`text-lg font-bold ${className}`}>{children}</h2>
-      {allowNew ? (
-        <button className='p-2' onClick={addSection}>
-          <PiPlusBold size={20} />
-        </button>
-      ) : null}
-    </div>
+    <>
+      <div
+        className='relative flex cursor-pointer items-center'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <h2 className={`text-lg font-bold ${className}`}>{children}</h2>
+        {allowNew && isHovered ? (
+          <button
+            className='ml-2 inline-block opacity-0 transition-opacity duration-200'
+            style={{ opacity: isHovered ? 1 : 0 }}
+            onClick={addSection}
+          >
+            <PiPlusBold size={20} />
+          </button>
+        ) : null}
+      </div>
+    </>
   );
 }
